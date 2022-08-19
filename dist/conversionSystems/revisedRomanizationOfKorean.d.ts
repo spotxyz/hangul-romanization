82efaf5015c2dc61fc19ee56a50e6d138c139e89
Sam Magura <srmagura@gmail.com>
2022-08-19 11:33:05 -0400

    Fixes

--- dist/conversionSystems/revisedRomanizationOfKorean.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @license MIT Copyright 2016 Daniel Imms (http://www.growingwiththeweb.com)
- */
-import { IHangulConversionSystem } from './hangulConversionSystem';
-export declare const REVISED_ROMANIZATION_OF_KOREAN: IHangulConversionSystem;
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @license MIT Copyright 2016 Daniel Imms (http://www.growingwiththeweb.com)
- */
-export declare function convert(text: string): string;
